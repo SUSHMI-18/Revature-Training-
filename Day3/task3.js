c905decf6a8a4c291bfb19a4ef1060cc2921dda3
SUSHMITHA M <163825801+SUSHMI-18@users.noreply.github.com>
2024-03-20 14:18:57 +0530

    Add files via upload

---
+++ Day3/task3.js
@@ -0,0 +1,19 @@
+function printPrimeNumbers(n) {
+    function isPrime(num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    for (let i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            console.log(i);
+        }
+    }
+}
+printPrimeNumbers(20); 
